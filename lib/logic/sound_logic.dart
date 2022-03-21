a1d95f289d429f7e5fb74b21b49791cf45cc8458
seiya endo <seiya.endo0830@gmail.com>
2022-03-21 14:38:36 +0900

    ボタンタップで音を出す実装

--- lib/logic/sound_logic.dart
+++ lib/logic/sound_logic.dart
@@ -1,9 +1,9 @@
 import 'package:audioplayers/audioplayers.dart';
 
 class SoundLogic {
-  static const soundDataUp = 'assets/sounds/sound_data_up.mp3';
-  static const soundDataDown = 'assets/sounds/sound_data_down.mp3';
-  static const soundDataReset = 'assets/sounds/sound_data_reset.mp3';
+  static const soundDataUp = 'sounds/sound_data_up.mp3';
+  static const soundDataDown = 'sounds/sound_data_down.mp3';
+  static const soundDataReset = 'sounds/sound_data_reset.mp3';
 
   final AudioCache _cache = AudioCache(
     fixedPlayer: AudioPlayer(),
--- lib/view_model.dart
+++ lib/view_model.dart
@@ -1,14 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_countup/logic/count_data_logic.dart';
+import 'package:riverpod_countup/logic/sound_logic.dart';
 import 'package:riverpod_countup/provider.dart';
 
 class ViewModel {
   final CountDataLogic _countDataLogic = CountDataLogic();
+  final SoundLogic _soundLogic = SoundLogic();
 
   late WidgetRef _ref;
 
   void setRef(WidgetRef ref) {
     _ref = ref;
+    _soundLogic.load();
   }
 
   get count => _ref.watch(countDataProvider).count.toString();
@@ -25,6 +28,7 @@ class ViewModel {
     _ref
         .watch(countDataProvider.state)
         .update((state) => _countDataLogic.countData);
+    _soundLogic.playUpSound();
   }
 
   void onDecrease() {
@@ -32,6 +36,7 @@ class ViewModel {
     _ref
         .watch(countDataProvider.state)
         .update((state) => _countDataLogic.countData);
+    _soundLogic.playDownSound();
   }
 
   void onReset() {
@@ -39,5 +44,6 @@ class ViewModel {
     _ref
         .watch(countDataProvider.state)
         .update((state) => _countDataLogic.countData);
+    _soundLogic.playResetSound();
   }
 }
