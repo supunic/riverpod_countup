d2ad3cab2fd8a7307e63a4cff85ba39b8a338699
seiya endo <seiya.endo0830@gmail.com>
2022-03-21 11:44:38 +0900

    View（Widget） をカウントアップ・ダウンどちらもできるよう修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_countup/provider.dart';
@@ -54,6 +55,32 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
               ref.watch(countProvider).toString(),
               style: Theme.of(context).textTheme.headline4,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                FloatingActionButton(
+                    // ref.watch -> ref.read にすると、button の rebuild がされなくなる
+                    onPressed: () => ref
+                        .read(countProvider.state)
+                        .update((state) => state + 1),
+                    tooltip: 'Increment',
+                    child: const Icon(CupertinoIcons.plus)),
+                FloatingActionButton(
+                    // ref.watch -> ref.read にすると、button の rebuild がされなくなる
+                    onPressed: () => ref
+                        .read(countProvider.state)
+                        .update((state) => state + 1),
+                    tooltip: 'Decrement',
+                    child: const Icon(CupertinoIcons.minus))
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                Text('1'),
+                Text('2'),
+              ],
+            ),
           ],
         ),
       ),
@@ -61,8 +88,8 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
           // ref.watch -> ref.read にすると、button の rebuild がされなくなる
           onPressed: () =>
               ref.read(countProvider.state).update((state) => state + 1),
-          tooltip: 'Increment',
-          child: const Icon(Icons.add)),
+          tooltip: 'Refresh',
+          child: const Icon(Icons.refresh)),
     );
   }
 }
