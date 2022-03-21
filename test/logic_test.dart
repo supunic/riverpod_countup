13923b9c36838404fb9fb67ea4e0a50b8070fc4b
seiya endo <seiya.endo0830@gmail.com>
2022-03-21 12:49:04 +0900

    viewModel（logic）のユニットテスト実装

--- test/logic_test.dart
+++ test/logic_test.dart
@@ -14,9 +14,45 @@ void main() {
     expect(target.countDown, 0);
   });
 
-  test('increase', () async {});
+  test('increase', () async {
+    target.increase();
+    expect(target.count, 1);
+    expect(target.countUp, 1);
+    expect(target.countDown, 0);
 
-  test('decrease', () async {});
+    target.increase();
+    target.increase();
+    target.increase();
+    expect(target.count, 4);
+    expect(target.countUp, 4);
+    expect(target.countDown, 0);
+  });
 
-  test('reset', () async {});
+  test('decrease', () async {
+    target.decrease();
+    expect(target.count, -1);
+    expect(target.countUp, 0);
+    expect(target.countDown, 1);
+
+    target.decrease();
+    target.decrease();
+    target.decrease();
+    expect(target.count, -4);
+    expect(target.countUp, 0);
+    expect(target.countDown, 4);
+  });
+
+  test('reset', () async {
+    target.increase();
+    target.increase();
+    target.decrease();
+    expect(target.count, 1);
+    expect(target.countUp, 2);
+    expect(target.countDown, 1);
+
+    target.reset();
+    expect(target.count, 0);
+    expect(target.countUp, 0);
+    expect(target.countDown, 0);
+  });
 }
