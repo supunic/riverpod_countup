de51229bdc07a18e10af7fefd61579c98a5ce2e2
seiya endo <seiya.endo0830@gmail.com>
2022-03-22 14:41:58 +0900

    provider 書き方修正

--- lib/provider/model/count_data_provider.dart
+++ lib/provider/model/count_data_provider.dart
@@ -3,5 +3,4 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../model/count_data/count_data.dart';
 
 final countDataProvider = StateProvider<CountData>(
-  (ref) => const CountData(count: 0, countUp: 0, countDown: 0),
-);
+    (ref) => const CountData(count: 0, countUp: 0, countDown: 0));
