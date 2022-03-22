aaea8fc7c311afff0fa8a2ded1ccf4269f1c36dd
seiya endo <seiya.endo0830@gmail.com>
2022-03-22 17:25:00 +0900

    DI 用に client と repository の provider を作成

--- lib/provider/model/postal_code_provider.dart
+++ lib/provider/model/postal_code_provider.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_countup/repository/api/postal_code_client.dart';
+import 'package:riverpod_countup/repository/api/postal_code_repository_impl.dart';
 
 import '../../model/postal_code/postal_code.dart';
 import '../../view_model/search_view_model.dart';
@@ -6,7 +8,7 @@ import '../../view_model/search_view_model.dart';
 final SearchViewModel _searchViewModel = SearchViewModel();
 
 final postalCodeProvider =
-    StateProvider<PostalCode>((ref) => const PostalCode(code: '', data: []));
+    StateProvider<PostalCode>((_) => const PostalCode(code: '', data: []));
 
 final postalCodeFutureProvider =
     FutureProvider<PostalCode>(_searchViewModel.onPostalCodeChange);
@@ -14,3 +16,9 @@ final postalCodeFutureProvider =
 final postalCodeFutureFamilyProvider =
     AutoDisposeFutureProviderFamily<PostalCode, String>(
         _searchViewModel.onPostalCodeChangeByFamily);
+
+final postalCodeClientProvider =
+    Provider<PostalCodeClient>((_) => PostalCodeClientImpl());
+
+final postalCodeRepositoryProvider = Provider<PostalCodeRepository>(
+    (ref) => PostalCodeRepositoryImpl(ref.read(postalCodeClientProvider)));
