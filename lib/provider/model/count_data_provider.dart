eebf285fb9a48567a87c27dfde79e3fff9f516b5
seiya endo <seiya.endo0830@gmail.com>
2022-03-26 18:26:25 +0900

    chore

--- lib/provider/model/count_data_provider.dart
+++ lib/provider/model/count_data_provider.dart
@@ -3,4 +3,4 @@ import 'package:riverpod_countup/model/count_data/count_data.dart';
 import 'package:riverpod_countup/notifier/count_data_notifier.dart';
 
 final countDataNotifierProvider = StateNotifierProvider<CountDataNotifier, CountData>(
-    (ref) => CountDataNotifier());
+    (_) => CountDataNotifier());
