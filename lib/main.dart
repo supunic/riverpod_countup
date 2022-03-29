1bdb651826bb3367e5c51863f0bcf8d44fed7b1e
seiya endo <seiya.endo0830@gmail.com>
2022-03-29 18:51:50 +0900

    firebase 初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_countup/config/firebase_options.dart';
 import 'package:riverpod_countup/view/home_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.ios);
+
   // MyApp 内で ProviderScope を使えるように wrap
   runApp(const ProviderScope(child: MyApp()));
 }
