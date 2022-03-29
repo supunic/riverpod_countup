064065f9ed041a129e9a7bab78a12e78cc6386ff
seiya endo <seiya.endo0830@gmail.com>
2022-03-29 20:39:50 +0900

    rename

--- lib/provider/model/postal_code_provider.dart
+++ lib/provider/model/postal_code_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_countup/model/postal_code/postal_code.dart';
 import 'package:riverpod_countup/notifier/postal_code_input_notifier.dart';
 import 'package:riverpod_countup/repository/api/postal_code_client.dart';
-import 'package:riverpod_countup/repository/api/postal_code_repository_impl.dart';
+import 'package:riverpod_countup/repository/api/postal_code_repository.dart';
 import 'package:riverpod_countup/view_model/search_view_model.dart';
 
 final postalCodeInputNotifierProvider =
